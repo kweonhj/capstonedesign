7841fa6caa1a59c964badf7861bff9effbfeab35
kweonhj <123445551+kweonhj@users.noreply.github.com>
2023-05-18 22:27:43 +0900

    Add files via upload

--- find_BnPfmincon.m
+++ find_BnPfmincon.m
@@ -1,7 +1,4 @@
-fclose('all');
-clc;
-clear;
-close all;
+fclose('all'); clc; clear; close all;
 
 % Define the measured magnetic field (B) at different positions
 % B = [Bx1, By1, Bz1; Bx2, By2, Bz2; ...]
@@ -11,13 +8,14 @@ close all;
 Bx = readmatrix('ax.txt');
 By = readmatrix('ay.txt');
 Bz = readmatrix('az.txt');
-B = zeros(size(Bx, 1), 3);
-Positions = zeros(size(Bx, 1), 3);
+B = zeros(size(Bx,1), 3);
+Positions = zeros(size(Bx,1), 3);
 
-for i = 1:size(B, 1)
-    B(i, :) = [Bx(i, 4), By(i, 4), Bz(i, 4)];
-    Positions(i, 1:3) = Bx(i, 1:3) .* 1e-3;
+for i = 1:size(B,1)
+    B(i, :) = [Bx(i,4), By(i,4), Bz(i,4)];
+    Positions(i,1:3) = Bx(i,1:3).*1e-3;
 end
+
 
 % Define the initial guess for the coil centroid position and magnetic moment
 initialGuess = [1, 1, 0, 1e-3, 1e-3, 1e-3]; % [x, y, z, mx, my, mz]
@@ -26,19 +24,21 @@ initialGuess = [1, 1, 0, 1e-3, 1e-3, 1e-3]; % [x, y, z, mx, my, mz]
 lb = [-inf, -inf, -inf, -inf, -inf, -inf];
 ub = [inf, inf, inf, inf, inf, inf];
 
-% Call lsqnonlin to perform the nonlinear least squares optimization
-options = optimoptions('lsqnonlin', 'Display', 'iter');
-options.FunctionTolerance = 1e-12;
+% Call fmincon to minimize the difference
+options = optimoptions('fmincon', 'Display', 'iter');
+options.OptimalityTolerance = 1e-12;
 options.StepTolerance = 1e-12;
-[optVariables, ~, residuals] = lsqnonlin(@(variables) objectiveFunction(variables, B, Positions), initialGuess, lb, ub, options);
+options.MaxFunctionEvaluations = 3e3;
+[optVariables, ~] = fmincon(@(variables) objectiveFunction(variables, B, Positions), initialGuess, [], [], [], [], lb, ub, @(variables) constraintFunction(variables, B, Positions), options);
 
 % Extract the optimized variables
 optCentroid = optVariables(1:3);
 optMagneticMoment = optVariables(4:6);
 
 % Calculate R-squared
-SSres = sum(residuals.^2);  % Sum of squares of residuals
-SStot = sum((B - mean(B)).^2);  % Total sum of squares
+B_predicted = calculateB(Positions, optCentroid, optMagneticMoment);
+SSres = sum((B - B_predicted).^2, 'all'); % Sum of squares of residuals
+SStot = sum((B - mean(B)).^2, 'all'); % Total sum of squares
 rsquared = 1 - SSres / SStot;
 
 % Display the optimized results
@@ -50,11 +50,18 @@ disp('R-squared:');
 disp(rsquared);
 
 % Objective function: Minimize the difference between measured B and B(r)
-function error = objectiveFunction(variables, B, Positions)
+function cost = objectiveFunction(variables, B, Positions)
     centroid = variables(1:3);
     magneticMoment = variables(4:6);
     B_predicted = calculateB(Positions, centroid, magneticMoment);
-    error = B - B_predicted;
+    error = sum(abs(B - B_predicted), 2).^2;
+    cost = sum(error, 'all');
+end
+
+% Constraint function: Empty in this case (no constraints)
+function [c, ceq] = constraintFunction(~, ~, ~)
+    c = [];
+    ceq = [];
 end
 
 % Function to calculate the theoretical magnetic field B(r)
